a886abc128d58285ae3575be88df5b5b8e0023a2
Trupti100 <127935389+Trupti100@users.noreply.github.com>
2023-09-08 08:32:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-// complete the given function
-
-function palindrome(str){
-
+function isPalindrome(s) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+  
+  // Reverse the string
+  const reversed = s.split('').reverse().join('');
+  
+  // Check if the original and reversed strings are the same
+  return s === reversed;
 }
-module.exports = palindrome
+
+// Test cases
+console.log(isPalindrome("race a car")); // false
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("RaceCar")); // true
+console.log(isPalindrome("raceCAR")); // true
